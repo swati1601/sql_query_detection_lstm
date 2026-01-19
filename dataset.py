f07fd1ad361fd4142be8819e2d05d0099e277c02
swati1601 <swatijadhav8476@gmail.com>
2026-01-19 12:05:06 +0530

    Remove all files from remote

--- dataset.py
+++
@@ -1,9 +0,0 @@
-#load dataset
-import numpy as np
-import pandas as pd
-
-data = pd.read_csv("sql_injection_dataset.csv")
-
-queries = data["query"].values
-labels = data["label"].values
-
--- datasetload.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-
-data = pd.read_csv("sql_injection_dataset.csv")
-
-queries = data["query"].values
-labels = data["label"].values
-
